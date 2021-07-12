9098a2ad0a25a1634dbbdf1b6b8b149bacb3493d
Bashar Ovi <bashar.ovi@outlook.com>
2021-07-12 23:51:50 +0600

    added mapbox component

--- AzureTS.ClientApp/portal/src/app/app.module.ts
+++ AzureTS.ClientApp/portal/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MapBoxComponent } from './map-box/map-box.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapBoxComponent
   ],
   imports: [
     BrowserModule,
