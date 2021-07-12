97a7dd6d4aa5364970c44b1ed5049404f3968c70
Bashar Ovi <bashar.ovi@outlook.com>
2021-07-13 01:23:06 +0600

    showed map on UI

---
+++ AzureTS.ClientApp/portal/src/app/environments/environment.ts
@@ -0,0 +1,11 @@
+export const mapConstants =
+{
+  mapCenter: [122.7875, -0.9516],
+  lan: -122.486052,
+  lat: 37.830348,
+  mapZoom: 15,
+  maxSiteShowInMultiSelectDropdown: 5,
+//   style: 'mapbox://styles/louisphan/cjx1hsiii17ng1cqy85xmrw20',
+  style: 'mapbox://styles/mapbox/streets-v11',
+  accessToken: 'pk.eyJ1Ijoic2ZtcyIsImEiOiJjand2czlxMzIwNGZsNDRwZGx2c3gwNWExIn0.aZXFKeOd42x8kkvl1udyKQ'
+};
--- AzureTS.ClientApp/portal/src/app/map-box/map-box.component.ts
+++ AzureTS.ClientApp/portal/src/app/map-box/map-box.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
+import { mapConstants as mapConst } from './../environments/environment';
 
 @Component({
   selector: 'app-map-box',
@@ -7,9 +9,78 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapBoxComponent implements OnInit {
 
+  map: any;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.initializeMap();
+
+    this.addGeoJsonLine();
+  }
+
+  initializeMap() {
+
+    this.map = new mapboxgl.Map({
+      accessToken: mapConst.accessToken,
+      container: "map",
+      style: mapConst.style,
+      center: [mapConst.lan, mapConst.lat],
+      zoom: mapConst.mapZoom,
+      attributionControl: false,
+    });
+
+  }
+
+  addGeoJsonLine() {
+    this.map.on('load', () => {
+      this.map.addSource('route', {
+        'type': 'geojson',
+        'data': {
+          'type': 'Feature',
+          'properties': {},
+          'geometry': {
+            'type': 'LineString',
+            'coordinates': [
+              [-122.483696, 37.833818],
+              [-122.483482, 37.833174],
+              [-122.483396, 37.8327],
+              [-122.483568, 37.832056],
+              [-122.48404, 37.831141],
+              [-122.48404, 37.830497],
+              [-122.483482, 37.82992],
+              [-122.483568, 37.829548],
+              [-122.48507, 37.829446],
+              [-122.4861, 37.828802],
+              [-122.486958, 37.82931],
+              [-122.487001, 37.830802],
+              [-122.487516, 37.831683],
+              [-122.488031, 37.832158],
+              [-122.488889, 37.832971],
+              [-122.489876, 37.832632],
+              [-122.490434, 37.832937],
+              [-122.49125, 37.832429],
+              [-122.491636, 37.832564],
+              [-122.492237, 37.833378],
+              [-122.493782, 37.833683]
+            ]
+          }
+        }
+      });
+      this.map.addLayer({
+        'id': 'route',
+        'type': 'line',
+        'source': 'route',
+        'layout': {
+          'line-join': 'round',
+          'line-cap': 'round'
+        },
+        'paint': {
+          'line-color': '#888',
+          'line-width': 8
+        }
+      });
+    });
   }
 
 }
