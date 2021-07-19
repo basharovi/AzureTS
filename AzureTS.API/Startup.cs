27de5ecc61834eb18d7be36849b4113ea6edd8b3
Bashar Ovi <bashar.ovi@outlook.com>
2021-07-19 22:01:45 +0600

    updated dockerfile

--- AzureTS.API/Startup.cs
+++ AzureTS.API/Startup.cs
@@ -40,11 +40,12 @@ namespace AzureTS.API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "AzureTS.API v1"));
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "AzureTS.API v1"));
             }
 
             app.UseCors(options =>
